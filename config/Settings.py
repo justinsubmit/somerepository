27457cd0b36c16948e8bee39be985c5dabd1c368
justinsubmit <justinsubmit@gmail.com>
2021-08-19 21:37:40 +0800

    test

--- config/Settings.py
+++ config/Settings.py
@@ -4,17 +4,17 @@ class Settings:
     secretKey="a12nc)238OmPq#cxOlm*a"
 
     #Dev
-    host='localhost'
-    database='furniture'
-    user='root'
-    password='Singapore1'
+    # host='localhost'
+    # database='furniture'
+    # user='root'
+    # password='Singapore1'
 
     #Production
-    # host=os.environ['HOST2']
-    # database=os.environ['DATABASE2']
-    # user=os.environ['USERNAME2']
-    # password=os.environ['PASSWORD2']
-    # print("host",host)
+    host=os.environ['HOST']
+    database=os.environ['DATABASE']
+    user=os.environ['USERNAME']
+    password=os.environ['PASSWORD']
+    print("host",host)
     print("database",database)
     print("username",user)
     print("password",password)
